c80fcba43783ef146c49d2864eabc497587c4aa4
Gerardo_Pascual_Francisco <al222011269@gmailc.om>
2021-12-01 16:52:31 -0600

    cambios7

--- routes/archivo.php
+++ routes/archivo.php
@@ -4,13 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<<<<<<< HEAD
-    <title>Docum
+    <title>Documto de 
 
-=======
-    <title>Documentbhbh
-        
->>>>>>> 25982f2934be9b88ce10b23e1be6fc63ca9ce5b0
     </title>
 </head>
 <body>
